dc7470ab3d74061ca44d2adbde6712862d2d2c01
Michal Moudrý <moudry.michal@live.com>
2026-09-18 22:38:26 +0200

    update main

--- rrt/bin/main.ml
+++ rrt/bin/main.ml
@@ -1,2 +1,17 @@
+let usage_msg = "rrt []"
+
+let anon_fun value = print_endline ("Received: " ^ value)
+
+let help () = print_endline "This is a help info"
+
+let spec_list =
+  [
+    ("--help", Arg.Unit help, "Displays tool's help information")
+  ]
+
+let main () =
+  Arg.parse spec_list anon_fun usage_msg
+
 let () =
-  print_endline "Hello, World!"
+  print_endline "Running!";
+  main()
